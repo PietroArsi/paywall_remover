865c8cfecd0ec06884b4241fedbbe4d175890e0e
pietroarsi <arsipietro@gmail.com>
2022-07-06 10:59:36 +0200

    first

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 let prefix = "https://12ft.io/";
+let darkModeToggle = false;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ prefix });
+  chrome.storage.sync.set({ darkModeToggle });
   console.log(`Prefix registered: ${prefix}`);
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
 let button1 = document.getElementById("button1");
+let darkmode = document.getElementById("darkmode");
+
+chrome.storage.sync.get("darkModeToggle", ({ darkModeToggle }) => {
+    console.log("TEST");
+});
 
 button1.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -11,3 +16,15 @@ button1.addEventListener("click", async () => {
        });
     });
 });
+
+darkmode.addEventListener("click", async () => {
+    chrome.storage.sync.get("darkModeToggle", ({ darkModeToggle }) => {
+        console.log("TEST");
+    });
+});
+
+// function toggleDarkMode() {
+//     chrome.storage.sync.get("darkModeToggle", ({ darkModeToggle }) => {
+//       console.log("TEST")
+//     });
+// }
